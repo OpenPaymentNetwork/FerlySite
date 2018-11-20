6325afd3817311002c42fcfa470854903b88e823
bbwilkes <bbradleywilkes@gmail.com>
2018-11-20 13:00:47 -0700

    Hide test profiles

--- backend/views/appviews.py
+++ backend/views/appviews.py
@@ -9,6 +9,11 @@ from pyramid.view import view_config
 from backend.utils import get_device
 
 
+@view_config(name='recaptcha-sitekey', renderer='json')
+def recaptcha_sitekey(request):
+
+
+
 @view_config(name='users', renderer='json')
 def list_users(request):
     """Return a list of all Ferly users.
@@ -21,7 +26,7 @@ def list_users(request):
     user = device.user
     dbsession = request.dbsession
     users = dbsession.query(User).filter(User.id != user.id).filter(
-        ~User.id.in_([10, 11, 12, 71, 51])).all()
+        ~User.id.in_([10, 11, 71, 51, 89])).all()
     # User.device_id != request.params.get('device_id')).all()
     return [serialize_user(request, u) for u in users]
 
