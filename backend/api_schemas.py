5aad1192b6812456cc8016e5de980961613f291b
williamsdjeffrey <danny.williams@aggiemail.usu.edu>
2019-09-17 15:07:13 -0600

    Added more email verification criteria

--- backend/api_schemas.py
+++ backend/api_schemas.py
@@ -15,7 +15,7 @@ import translationstring
 _ = translationstring.TranslationStringFactory('colander')
 phone_detect_re = re.compile(r'^[(+\s]*[0-9]')
 EMAIL_RE = (
-    r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9]+[.]"
+    r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9]+[.]+[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]"
     r"(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9]"
     r"(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 )
